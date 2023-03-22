227a6826dc4c137268298d82f5b00ecd1019bab2
KevvKo <kklein915@gmail.com>
2023-03-22 09:33:16 +0100

    setup dependencies and add model

---
+++ src/app/core/models/material.ts
@@ -0,0 +1,18 @@
+export interface material {
+    Naterial: number;
+    Quantity: number;
+    DescTxt: string;
+    CustomerPrice: string;
+    CustomerCurrency: string;
+    RepairPrice: string;
+    RepairCurrency: string;
+    Available: boolean;
+    ExtUnit: string;
+    MatStatus: string;
+    StorageLoc: number;
+    StorageLocDesc: number;
+    NDFQuote: string;
+    NDFCounter: string;
+    TSPercentage: string;
+    TSPercentageCounter:  string;
+}
