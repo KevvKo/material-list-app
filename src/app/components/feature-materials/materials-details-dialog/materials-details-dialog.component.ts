6e6e3a9ad0b73b5390f8dcdfc3c6a674dc6770bb
KevvKo <kklein915@gmail.com>
2023-03-27 14:12:25 +0200

    refactor subscriptions

--- src/app/components/feature-materials/materials-details-dialog/materials-details-dialog.component.ts
+++ src/app/components/feature-materials/materials-details-dialog/materials-details-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MaterialModel } from 'src/app/core/models';
 import { MaterialService } from 'src/app/core/services/material-service/material.service';
+import { Subscription, forkJoin, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-materials-details-dialog',
@@ -9,35 +10,40 @@ import { MaterialService } from 'src/app/core/services/material-service/material
 
 export class MaterialsDetailsDialogComponent implements OnInit, OnDestroy {
 
+  selectedMaterialsSubscription: Subscription;
+  materialsSubscription: Subscription;
+
   selectedMaterial: MaterialModel | null
   materials: MaterialModel[]
+  materialsLength: number;
+  selectedMaterialIndex: number;
 
   constructor(private materialService: MaterialService) {}
 
   ngOnInit(): void {
-      this.materialService.currentSelectedMaterial.subscribe((item) => this.selectedMaterial = item)
-      this.materialService.materials.subscribe((data) => this.materials = data)
+      this.selectedMaterialsSubscription = this.materialService.currentSelectedMaterial.subscribe((item) => this.selectedMaterial = item);
+      this.materialsSubscription = this.materialService.materials.subscribe((data) => this.materials = data);
+      this.materialsLength = this.materials.length;
+      this.selectedMaterialIndex = this.materials.findIndex((item) => this.selectedMaterial?.Id === item.Id)
   }
 
   ngOnDestroy(): void {
     this.materialService.removeSelectedMaterial()
+    this.selectedMaterialsSubscription.unsubscribe();
+    this.materialsSubscription.unsubscribe();
   }
 
   public handleClickRight(): void {
-    const length = this.materials.length
-    let index = this.materials.findIndex((item) => this.selectedMaterial?.Id === item.Id)
-
-    index === length -1 ? index = 0 : index += 1
-
-    this.materialService.updateSelectedMaterial( this.materials[index])
+    this.selectedMaterialIndex === this.materialsLength -1 
+      ?  this.selectedMaterialIndex = 0 
+      :  this.selectedMaterialIndex += 1
+    this.materialService.updateSelectedMaterial( this.materials[this.selectedMaterialIndex])
   }
 
   public handleClickLeft(): void {
-    const length = this.materials.length
-    let index = this.materials.findIndex((item) => this.selectedMaterial?.Id === item.Id)
-
-    index === 0 ? index = length -1 : index -= 1
-
-    this.materialService.updateSelectedMaterial( this.materials[index])
+    this.selectedMaterialIndex === 0 
+      ?  this.selectedMaterialIndex = this.materialsLength  -1 
+      :  this.selectedMaterialIndex -= 1
+    this.materialService.updateSelectedMaterial( this.materials[this.selectedMaterialIndex])
   }
 }
